6a2a169770ea00b678cd10e50ebc1735e8c1b2e6
t5672918 <idobron123@gmail.com>
2014-04-03 14:47:23 +0300

    test

--- lib/Entry.php
+++ lib/Entry.php
@@ -10,22 +10,34 @@ namespace U443;
 
 
 class Entry {
-    private $_app, $_table, $_columns; // Columns is a dictionary of name : type
+    private $_app, $_table, $_columns, $_keys; // Columns is a dictionary of name : type
 
     public function __constructor ($keys) {
+        $this->$_keys =  $keys;
         // $keys is a dictionary of name : value
         // Returns a Entry object of the created row
         // Performs INSERT using U443::send()
     }
 
     public function __set($name, $value) {
-        // Generic setter, must check input validity
-        // Performs update if needed
+        if (!isset($this->_columns[$name]))
+        {
+            //ERROR!
+        }
+        if (gettype($this->_columns[$name]) != $value)
+        {
+            //ERROR!
+        }
+
     }
 
     public function __get($name) {
         // Generic getter, must check input validity
         // All values should already be set locally, no SQL needed
+        if (!isset($this->_columns[$name]))
+        {
+            //ERROR!
+        }
     }
 
     public function remove() {
