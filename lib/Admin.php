2b46b094ebe5a4b23babd5663589ca4f5e59e52c
Guy Weizman <gweizman@gmail.com>
2014-04-06 23:50:53 +0300

    Fuck you all.

---
+++ lib/Admin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guy Weizman
+ * Date: 06/04/14
+ * Time: 23:50
+ */
+
+
+class Admin {
+
+    public static function createApp($api,$name) {
+        // Returns app key
+    }
+
+    public static function deleteApp($api, $name) {
+        //Test!!!!!!!!!
+    }
+
+    public static function setPermissionGroupAdmin($api, $user) {
+        // setPermissionGroupAdmin
+    }
+
+    public static function addPermissionGroup($api, $name) {
+
+    }
+
+    public static function removePermissionGroup($api, $name) {
+
+    }
+
+    public static function addPermissionGroupForTable($api, $name, $to, $type) {
+
+    }
+
+    public static function removePermissionGroupForTable($api, $name, $frome, $type) {
+
+    }
+}
