668d8bab0d9959278f13254f5d495c73be591ab9
gweizman <gweizman@gmail.com>
2014-04-09 00:49:28 +0300

    Update talnet.php
    def guard

--- lib/talnet.php
+++ lib/talnet.php
@@ -1,9 +1,12 @@
 <?php
-require_once ("Communicate.php");
-require_once ("Entry.php");
-//require_once ("Condition.php");
-require_once ("Table.php");
-//require_once ("User.php");
-
-define("TALNET_ENABLED", TRUE);
+if (!defined("TALNET_ENABLED"))
+{
+  require_once ("Communicate.php");
+  require_once ("Entry.php");
+  //require_once ("Condition.php");
+  require_once ("Table.php");
+  //require_once ("User.php");
+  
+  define("TALNET_ENABLED", TRUE);
+}
 ?>
