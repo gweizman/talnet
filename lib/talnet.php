0deac5dc5da990961d5317c1e9d3e0fbd2992157
Guy Weizman <gweizman@gmail.com>
2014-04-11 22:05:10 +0300

    Loaded everything in library. Might cause bugs.

--- lib/talnet.php
+++ lib/talnet.php
@@ -4,9 +4,9 @@ if (!defined("TALNET_ENABLED"))
   session_start();
   require_once ("Communicate.php");
   require_once ("Entry.php");
-  //require_once ("Condition.php");
+  require_once ("Condition.php");
   require_once ("Table.php");
-  //require_once ("User.php");
+  require_once ("User.php");
   
   define("TALNET_ENABLED", TRUE);
 }
