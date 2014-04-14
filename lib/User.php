a960706f345ad26e1b9502b8d519708080b9c27f
Guy Weizman <gweizman@gmail.com>
2014-04-14 18:02:45 +0300

    Make a guess

--- lib/User.php
+++ lib/User.php
@@ -63,7 +63,6 @@ class User extends Entry {
      */
     public static function get($condition) {
         $request = RequestFactory::createUserAction("SELECT", NULL , $condition);
-        return $request;
         $answer = Communicate::send(User::$_app,$request);
         return $answer;
     }
