cc3e1cf85c877d3a5df7770f7955687dc3ee307d
Guy Weizman <gweizman@gmail.com>
2014-04-15 22:23:41 +0300

    Them typos

--- lib/talnet.php
+++ lib/talnet.php
@@ -1,15 +1,9 @@
 <?php
+
 namespace talent;
 
 if (!defined("TALNET_ENABLED"))
 {
-    require_once ("Communicate.php");
-    require_once ("Entry.php");
-    require_once ("BaseCondition.php");
-    require_once ("Condition.php");
-    require_once ("Table.php");
-    require_once ("User.php");
-
     class Talnet {
         private static $_app;
         /**
@@ -28,6 +22,13 @@ if (!defined("TALNET_ENABLED"))
             );
         }
     }
+
+    require_once ("Communicate.php");
+    require_once ("Entry.php");
+    require_once ("BaseCondition.php");
+    require_once ("Condition.php");
+    require_once ("Table.php");
+    require_once ("User.php");
 
     session_start();
     define("TALNET_ENABLED", TRUE);
