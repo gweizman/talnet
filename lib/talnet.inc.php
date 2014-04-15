97b0f1998e95052bdfd41ecd5942669638f3d429
Guy Weizman <gweizman@gmail.com>
2014-04-15 22:27:08 +0300

    moved class out of talnet.php

---
+++ lib/talnet.inc.php
@@ -0,0 +1,21 @@
+<?php
+namespace talent;
+
+class Talnet {
+    private static $_app;
+
+    /**
+     * Sets the current app used by the library. Must be called before any other function.
+     *
+     * @param $app array with 'name' and 'key' of valid apps
+     */
+    public static function setApp($app) {
+        Talnet::$_app = $app;
+    }
+    public static function getApp() {
+        return array(
+            'name' => Talnet::$_app['name'],
+            'key' => Talnet::$_app['key']
+        );
+    }
+}
--- lib/talnet.php
+++ lib/talnet.php
@@ -4,25 +4,7 @@ namespace talent;
 
 if (!defined("TALNET_ENABLED"))
 {
-    class Talnet {
-        private static $_app;
-        /**
-         * Sets the current app used by the library. Must be called before any other function.
-         *
-         * @param $app array with 'name' and 'key' of valid apps
-         */
-
-        public static function setApp($app) {
-            Talnet::$_app = $app;
-        }
-        public static function getApp() {
-            return array(
-                'name' => Talnet::$_app['name'],
-                'key' => Talnet::$_app['key']
-            );
-        }
-    }
-
+    require_once ("talnet.inc.php");
     require_once ("Communicate.php");
     require_once ("Entry.php");
     require_once ("BaseCondition.php");
