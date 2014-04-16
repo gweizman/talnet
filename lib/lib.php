fca1e07a37cc64a399fed5a42eb6a1b49faa1c22
Guy Weizman <gweizman@gmail.com>
2014-04-16 17:56:24 +0300

    Loaded all files.

--- lib/lib.php
+++ lib/lib.php
@@ -4,6 +4,7 @@ namespace talent;
 
 if (!defined("TALNET_ENABLED"))
 {
+    require_once("RequestFactory.php");
     require_once ("Talnet.php");
     require_once ("Communicate.php");
     require_once ("Entry.php");
@@ -11,6 +12,10 @@ if (!defined("TALNET_ENABLED"))
     require_once ("Condition.php");
     require_once ("Table.php");
     require_once ("User.php");
+    require_once("App.php");
+    require_once("Column.php");
+    require_once("Permission.php");
+
 
     session_start();
     define("TALNET_ENABLED", TRUE);
