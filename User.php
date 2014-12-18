08182ae2d74bb5ac128d4781a746f539bb6675b5
GWeizman <gweizman@gmail.com>
2014-12-18 23:04:16 +0200

    User::sendMail formatting

--- User.php
+++ User.php
@@ -170,7 +170,7 @@ class User extends Entry
             ->setFrom(array('talnet.talpiot@gmail.com' => 'Talnet'))
             ->setTo(array($this->EMAIL))
             ->setBody("
-            <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html><head>
+            <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html dir='rtl' lang='he'><head>
     <title></title>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
     <style type='text/css'>
