3477f2337b86f92de912d2dbaed8e759ac3815bd
gweizman <gweizman@gmail.com>
2015-01-05 11:26:23 +0200

    Update User.php

--- User.php
+++ User.php
@@ -144,13 +144,11 @@ class User extends Entry
     {
         $request = RequestFactory::createUserAction("GET_GROUPS");
         $answer = $this->_app->send($request);
-        print_r($answer);
         $permissions = array();
         foreach ($answer as $permission) {
             array_push($permissions, new Permission($permission, false));
         }
-        print_r($permission);
-        return $permission;
+        return $permissions;
     }
 
     /**
